24ee03f47ebd896fb3427d99dd47b444c7c7e882
Matheus Faria <123787187+MatheusGiove@users.noreply.github.com>
2023-06-12 17:28:58 -0300

    menu mobile update

--- animais-fantásticos/js/modules/outsideClick.js
+++ animais-fantásticos/js/modules/outsideClick.js
@@ -4,16 +4,16 @@ export default function outsideClick(elemento, eventos, callback) {
   if (!elemento.hasAttribute(outside)) {
     eventos.forEach(evento => {
       setTimeout(() => html.addEventListener(evento, cliqueFora))
-    })
+    }, 0)
     elemento.setAttribute(outside, "")
   }
   function cliqueFora(evento) {
-    if (!elemento.contains(evento.currentTarget)){
+    if (!elemento.contains(evento.target)){
       elemento.removeAttribute(outside)
       eventos.forEach((evento) => {
         html.removeEventListener(evento, cliqueFora)
       })
+      callback()
     }
-    callback()
   }
 }
