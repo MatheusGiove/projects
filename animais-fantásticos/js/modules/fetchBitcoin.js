b3dfe71f3e313ffc440c5ad790372665f1ce0ad6
Matheus Faria Giove <123787187+MatheusGiove@users.noreply.github.com>
2023-07-04 20:11:31 -0300

    update fetchBitcoin

--- animais-fantásticos/js/modules/fetchBitcoin.js
+++ animais-fantásticos/js/modules/fetchBitcoin.js
@@ -1,13 +1,11 @@
 export default function initfetchBitcoin() {
-
+  fetch("https://blockchain.info/ticker")
+  .then(response => response.json())
+  .then(bitcoin => {
+    const btcPreco = document.querySelector(".btc-preco")
+    btcPreco.innerHTML = (1000 / bitcoin.BRL.sell).toFixed(4)
+  })
+  .catch(erro => {
+    console.log(Error(`${erro} Bitcoin`))
+  })
 }
-
-fetch("https://blockchain.info/ticker")
-.then(response => response.json())
-.then(bitcoin => {
-  const btcPreco = document.querySelector(".btc-preco")
-  btcPreco.innerHTML = (1000 / bitcoin.BRL.sell).toFixed(4)
-})
-.catch(erro => {
-  console.log(Error(`${erro} Bitcoin`))
-})
