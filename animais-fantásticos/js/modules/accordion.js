3f8f2fa994b8d177a55f50214bd18676bf5c30b0
Matheus Faria Giove <123787187+MatheusGiove@users.noreply.github.com>
2023-07-04 21:53:09 -0300

    delete eslint

--- animais-fantásticos/js/modules/accordion.js
+++ animais-fantásticos/js/modules/accordion.js
@@ -1,13 +1,13 @@
 export default function accordion() {
   const lista = document.querySelectorAll("[data-accordion='accordion'] dt");
   const ativo = "ativo";
+  function ativarAccordion() {
+    this.classList.toggle(ativo);
+    this.nextElementSibling.classList.toggle(ativo);
+  }
   if (lista.length) {
     lista[0].classList.add(ativo);
     lista[0].nextElementSibling.classList.add(ativo);
-    function ativarAccordion() {
-      this.classList.toggle(ativo);
-      this.nextElementSibling.classList.toggle(ativo);
-    }
     lista.forEach((pergunta) => {
       pergunta.addEventListener("click", ativarAccordion);
     });
