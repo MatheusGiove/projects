4b9b9d2a2c9f22d5e05dfabd4a5b51e4202b70aa
Matheus Faria <123787187+MatheusGiove@users.noreply.github.com>
2023-07-11 23:00:02 -0300

    update refatoracao do fetch dos animais

--- animais-fantásticos/js/modules/fetchAnimais.js
+++ animais-fantásticos/js/modules/fetchAnimais.js
@@ -1,26 +1,34 @@
-import animaNumeros from "./animaNumeros.js";
+import AnimaNumeros from "./animaNumeros.js";
 
-export default function initfetchAnimais() {
-  function createAnimals(animais) {
+export default function fetchAnimais(url, target) {
+  function criarAnimalElemento(animais) {
     const div = document.createElement("div");
     div.classList.add("numero-animal");
     div.innerHTML = `<h3>${animais.especie}</h3><span data-numero>${animais.total}</span>`;
     return div;
   }
-  async function fetchAnimais(url) {
+
+  const numerosGrid = document.querySelector(target);
+  function preencherAnimais(animal) {
+    const animaisDiv = criarAnimalElemento(animal);
+    numerosGrid.appendChild(animaisDiv);
+  }
+
+  function animaAnimaisNumero() {
+    const animaNumeros = new AnimaNumeros("[data-numero]", ".numeros", "ativo");
+    animaNumeros.init();
+  }
+
+  async function criarAnimais() {
     try {
       const animaisReponse = await fetch(url);
       const animaisJson = await animaisReponse.json();
-      const numerosGrid = document.querySelector(".numeros-grid");
-      animaisJson.forEach((animais) => {
-        const animaisDiv = createAnimals(animais);
-        numerosGrid.appendChild(animaisDiv);
-      });
-      animaNumeros();
-    }
-    catch (erro) {
-      console.log(erro)
+      animaisJson.forEach((animal) => preencherAnimais(animal));
+      animaAnimaisNumero()
+    } catch (erro) {
+      console.log(erro);
     }
   }
-  fetchAnimais("./animaisapi.json");
+
+  return criarAnimais();
 }
