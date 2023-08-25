17f9dac0844872c80a45a3f78d64bdbfe33eb29a
Matheus Faria Giove <123787187+MatheusGiove@users.noreply.github.com>
2023-08-25 03:37:24 -0300

    update

--- to-do-list/index.js
+++ to-do-list/index.js
@@ -1,10 +1,32 @@
-const ola = () => {
-  console.log(this)
-}
+class CriarCategoria {
+  constructor () {
+    this.categoria = document.querySelector('#criarCategoria')
+    this.adicionar = document.querySelector('#adicionarCategoria')
+  }
 
-function ola2() {
-  console.log(this)
-}
+  pegarValorCategoria(valorDaCategoria) {
+    return valorDaCategoria
+  }
+  
+  adicionarNovaCategoria() {
+    const article = document.createElement('article')
+    const title = document.createElement("h1")
+    title.appendChild(article)
+    if (this.categoria.value) {
+      console.log('Valor verdadeiro')
+    } else {
+      console.log('Valor falso')
+    }
 
-console.log(ola())
-console.log(ola2())
+  }
+  adicionandoCategoria() {
+    this.adicionar.addEventListener('click', this.adicionarNovaCategoria)
+  }
+  init() {
+    console.log(this.adicionar)
+    console.log(this.categoria)
+    this.adicionandoCategoria()
+    return this
+  }
+}
+const adicionar = new CriarCategoria().init()
