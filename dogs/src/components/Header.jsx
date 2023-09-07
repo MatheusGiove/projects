eb61e7178d4ecd194201d069b839f420a078e4fa
Matheus Faria <123787187+MatheusGiove@users.noreply.github.com>
2023-09-07 15:54:46 -0300

    correção de bug

--- dogs/src/components/Header.jsx
+++ dogs/src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
-import { ReactComponent as Dogs } from "../Assets/dogs.svg";
+import { ReactComponent as Dogs } from "../assets/dogs.svg";
 import { UserContext } from "../UserContext";
 
 const Header = () => {
