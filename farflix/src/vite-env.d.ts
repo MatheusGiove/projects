92af84e1409781fd9eeae8087ab568c2efc190f2
Matheus Faria <123787187+MatheusGiove@users.noreply.github.com>
2023-09-22 13:40:50 -0300

    update d.ts

--- farflix/src/vite-env.d.ts
+++ farflix/src/vite-env.d.ts
@@ -3,14 +3,14 @@
 interface ResultsGetFilms {
   adult: boolean; 
   backdrop_path: string;
-  first_air_date?: string;
+  first_air_date: string;
   genres_ids: Array<number>;
   id: number;
-  media_type?: string;
-  name?: string;
-  origin_country?: Array<string>;
+  media_type: string;
+  name: string;
+  origin_country: Array<string>;
   original_language: string;
-  original_name?: string;
+  original_name: string;
   original_title: string;
   overview: string;
   popularity: number;
