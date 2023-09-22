264844709fa805a13136a5629fce3bb4e378e5ba
Matheus Faria <123787187+MatheusGiove@users.noreply.github.com>
2023-09-22 15:11:10 -0300

    update header

--- farflix/src/Pages/Home/Home.tsx
+++ farflix/src/Pages/Home/Home.tsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react";
+import getDatabase from "../../Services/Tmdb";
+import Loading from "../../Helper/Loading";
+import Container from "../../Components/Container/Container";
+
 const Home = () => {
-  return (
-      <div>Home</div>
-  );
+  const [data, setData] = useState<Database[] | null>(null);
+
+  useEffect(() => {
+    const data = async () => {
+      const films = await getDatabase("pt-BR");
+      setData(films);
+    };
+    data();
+  }, []);
+
+  console.log(data);
+
+  if (!data) return <Loading />;
+  return <>{data.map(({items, title, url}) => (
+    <Container data={items} title={title} url={url} />
+  ))}</>;
 };
 
 export default Home;
