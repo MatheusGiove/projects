15a99e5582968b36a68063f582e663545f92fde8
Matheus Faria Giove <123787187+MatheusGiove@users.noreply.github.com>
2023-09-24 13:44:53 -0300

    update principal

--- farflix/src/Components/Principal/Principal.tsx
+++ farflix/src/Components/Principal/Principal.tsx
@@ -1,13 +1,6 @@
-import useFetch from "../../Hooks/useFetch";
 import styles from "./Principal.module.css";
-import { options } from "../../Services/Tmdb";
 
 const Principal = ({ language }: { language: string }) => {
-  const fetchToprated = useFetch(
-    `https://api.themoviedb.org/3/trending/all/week?language=${language}`,
-    options
-  );
-
   return (
     <section style={{ backgroundImage: "" }}>
       <div className={styles.principal}>Opa</div>
